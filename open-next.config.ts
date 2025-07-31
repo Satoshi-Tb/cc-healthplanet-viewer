11490295cd611de9b0663f5d9118c73d9eec9a3d
Satoshi T <64382011+Satoshi-Tb@users.noreply.github.com>
2025-07-31 22:57:55 +0900

    設定修正

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,9 +1,3 @@
-// default open-next.config.ts file created by @opennextjs/cloudflare
-import { defineCloudflareConfig } from "@opennextjs/cloudflare/config";
-export default defineCloudflareConfig({
-  // Uncomment to enable R2 cache,
-  // It should be imported as:
-  // `import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";`
-  // See https://opennext.js.org/cloudflare/caching for more details
-  // incrementalCache: r2IncrementalCache,
-});
+import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+
+export default defineCloudflareConfig();
